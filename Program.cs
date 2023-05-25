f8017c5320e4fb55ab4a6513675acb48e8a9492c
Robert Macias <Robert.Macias@trialcard.com>
2023-05-24 23:39:05 -0500

    feat: add support for Serilog

--- Program.cs
+++ Program.cs
@@ -1,32 +1,54 @@
+using Serilog;
 using CarvedRock.Api.Interfaces;
 using CarvedRock.Api.Domain;
 using CarvedRock.Api.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
+var name = typeof(Program).Assembly.GetName().Name;
 
-// Add services to the container.
-builder.Services.AddScoped<IProductLogic, ProductLogic>();
-builder.Services.AddScoped<IQuickOrderLogic, QuickOrderLogic>();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", name)
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+try {
 
-var app = builder.Build();
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseCustomExceptionHandler();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Host.UseSerilog(); // <-- Add Serilog support
+
+    // Add services to the container.
+    builder.Services.AddScoped<IProductLogic, ProductLogic>();
+    builder.Services.AddScoped<IQuickOrderLogic, QuickOrderLogic>();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseCustomExceptionHandler();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();    
+
+} catch (Exception ex) {
+    Log.Fatal(ex, "Application terminated unexpectedly");
+} finally {
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
