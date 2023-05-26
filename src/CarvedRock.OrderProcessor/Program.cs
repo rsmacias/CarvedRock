1b8246f654a6529475b505d08e7996d72cacf8cc
Robert Macias <rsmacias@gmail.com>
2023-05-26 10:55:41 -0500

    feat: add new projects and a solution file to attach them.

---
+++ src/CarvedRock.OrderProcessor/Program.cs
@@ -0,0 +1,10 @@
+using CarvedRock.OrderProcessor;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
