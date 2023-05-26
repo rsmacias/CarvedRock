63dbe17f18e46519001cba20584b56f26782bf83
Robert Macias <rsmacias@gmail.com>
2023-05-26 11:56:08 -0500

    feat: add Serilog support for the console project.

--- src/CarvedRock.InvoiceGenerator/Program.cs
+++ src/CarvedRock.InvoiceGenerator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+
+// See https://aka.ms/new-console-template for more information
+
+var name = typeof(Program).Assembly.GetName().Name;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", name)
+    .WriteTo.Seq(serverUrl: "http://host.docker.internal:5341")
+    .WriteTo.Console()
+    .CreateLogger();
+
+try {
+    Log.ForContext("Args", args)
+       .Information("Starting program...");
+
+    Console.WriteLine("Hello, World!");  // do some invoice generation
+
+    Log.Information("Finished execution!");
+} catch (Exception ex) {
+    Log.Fatal(ex, "Some kind of exception occured");
+} finally {
+    Log.CloseAndFlush();
+}
